ba714970e2e0ff6caeed8eb2ea0fac2b269d88c4
Kim Cole <kimacole70@gmail.com>
2017-03-06 17:44:46 -0600

    readded stringNlist assignment and added MultiplesSumAvg assignment

---
+++ kim_cole/Assignments/MultiplesSumAvg.py
@@ -0,0 +1,19 @@
+# Multiples:
+# Part I
+for count in range(1, 1001, 2):
+	print count
+
+# Part II
+for count in range (5, 1000001, 5):
+	print count
+
+# Sum list
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+
+# Average list
+a = [1, 2, 5, 10, 255, 3]
+Average = (sum(a))/(len(a))
+print	Average
+
+
